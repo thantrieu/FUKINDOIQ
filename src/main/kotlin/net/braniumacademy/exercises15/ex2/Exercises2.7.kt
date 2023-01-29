65c5dc5a65cd775109a7f2eda5eb0cbd909fca88
thantrieu <trieuthan92pro@gmail.com>
2023-01-29 17:09:56 +0700

    Add ex 2.7

--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.7.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.7.kt
@@ -1,2 +1,11 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.avg(): Float {
+    val sum = this.sumElements()
+    return sum * 1.0f / this.size
+}
+
+fun main() {
+    val numbers = arrayOf(-20, 3, 2, 100, 4, 7, 8, 9, -20, 0, 100, -20, -4)
+    println("TB: ${numbers.avg()}")
+}

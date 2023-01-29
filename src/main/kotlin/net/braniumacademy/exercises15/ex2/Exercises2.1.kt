60de9e548ec0e6e1a111c017d9b3797b97fb8b0f
thantrieu <trieuthan92pro@gmail.com>
2023-01-29 15:36:22 +0700

    Add update ex 2.1 + 2.2

--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.1.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.1.kt
@@ -1,2 +1,16 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.maxElement(): Int {
+    var max = this[0] // giả định giá trị max là phần tử đầu tiên trong mảng
+    for (item in this) { // tìm phần tử lớn hơn trong mảng
+        if (item > max) { // nếu tìm thấy thì cập nhật lại giá trị max
+            max = item
+        }
+    }
+    return max // trả về giá trị lớn nhất trong mảng
+}
+
+fun main() {
+    val numbers = arrayOf(1, 3, 2, 5, 4, 7, 8, 9, 6, 0, 100, -20, -54)
+    println("Giá trị lớn nhất trong mảng: ${numbers.maxElement()}")
+}
--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.2.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.2.kt
@@ -1,2 +1,16 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.minElement(): Int {
+    var min = this[0] // giả định giá trị min là phần tử đầu tiên trong mảng
+    for (item in this) { // tìm phần tử nhỏ hơn trong mảng
+        if (item < min) { // nếu tìm thấy thì cập nhật lại giá trị min
+            min = item
+        }
+    }
+    return min // trả về giá trị nhỏ nhất trong mảng
+}
+
+fun main() {
+    val numbers = arrayOf(1, 3, 2, 5, 4, 7, 8, 9, 6, 0, 100, -20, -54)
+    println("Giá trị nhỏ nhất trong mảng: ${numbers.minElement()}")
+}

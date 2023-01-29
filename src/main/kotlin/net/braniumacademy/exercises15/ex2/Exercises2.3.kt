6a36249534cf7d54b5985c5f549fc71007970a66
thantrieu <trieuthan92pro@gmail.com>
2023-01-29 17:04:32 +0700

    Add update ex 2.3 + 2.4

--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.3.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.3.kt
@@ -1,2 +1,17 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.countMaxElements(): Int {
+    val max = this.maxElement() // tìm giá trị lớn nhất trong mảng(đã làm ở ý 1)
+    var counter = 0 // biến đếm số lượng phần tử có giá trị max
+    for (item in this) { // tìm các phần tử trong mảng có giá trị bằng max
+        if (item == max) { // nếu tìm thấy thì tăng biến đếm lên
+            counter++
+        }
+    }
+    return counter // trả về kết quả
+}
+
+fun main() {
+    val numbers = arrayOf(100, 3, 2, 100, 4, 7, 8, 9, 100, 0, 100, -20, -54)
+    println("Số lượng phần tử có giá trị lớn nhất: ${numbers.countMaxElements()}")
+}
--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.4.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.4.kt
@@ -1,2 +1,17 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.countMinElements(): Int {
+    val min = this.minElement() // tìm giá trị nhỏ nhất trong mảng(đã làm ở ý 1)
+    var counter = 0 // biến đếm số lượng phần tử có giá trị min
+    for (item in this) { // tìm các phần tử trong mảng có giá trị bằng min
+        if (item == min) { // nếu tìm thấy thì tăng biến đếm lên
+            counter++
+        }
+    }
+    return counter // trả về kết quả
+}
+
+fun main() {
+    val numbers = arrayOf(-20, 3, 2, 100, 4, 7, 8, 9, -20, 0, 100, -20, -4)
+    println("Số lượng phần tử có giá trị nhỏ nhất: ${numbers.countMinElements()}")
+}

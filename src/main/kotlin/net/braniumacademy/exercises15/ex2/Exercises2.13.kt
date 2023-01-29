d77968da2c1e388017921c63ba900cc5d1900299
thantrieu <trieuthan92pro@gmail.com>
2023-01-29 21:31:10 +0700

    Add update ex 2.13 + 2.14

--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.13.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.13.kt
@@ -1,2 +1,18 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.primeNumbers(k: Int) {
+    this.sort() // sắp xếp để các phần tử theo thứ tự tăng dần
+    for (item in this) {
+        if (item > k && isPrime(item)) {
+            print("$item ")
+        }
+    }
+}
+
+fun main() {
+    val numbers = arrayOf(-20, 11, 2, 100, 505, 7, 803, 997, -2, 0, 16, 1111, 101)
+    println("Nhập k: ")
+    val k = readln().toInt()
+    println("Các số nguyên tố lớn hơn $k trong mảng:")
+    numbers.primeNumbers(k)
+}
--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.14.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.14.kt
@@ -1,2 +1,35 @@
 package net.braniumacademy.exercises15.ex2
 
+// hàm kiểm tra số fibonacci
+fun isFibonacci(n: Int): Boolean {
+    var fn = 0
+    var f0 = 0
+    var f1 = 1
+    while (fn <= n) {
+        fn = f0 + f1
+        if (fn == n) {
+            return true
+        }
+        f0 = f1
+        f1 = fn
+    }
+    return false
+}
+
+// hàm liệt kê các số fibonacci lớn hơn k
+fun Array<Int>.fibonacciNumbers(k: Int) {
+    this.sort() // sắp xếp để các phần tử theo thứ tự tăng dần
+    for (item in this) {
+        if (item > k && isFibonacci(item)) {
+            print("$item ")
+        }
+    }
+}
+
+fun main() {
+    val numbers = arrayOf(0, 144, 1, 233, 2, 3, 5, 8, 13, 89, 55, 34, 16, 21, 101)
+    println("Nhập k: ")
+    val k = readln().toInt()
+    println("Các số fibonacci lớn hơn $k trong mảng:")
+    numbers.fibonacciNumbers(k)
+}

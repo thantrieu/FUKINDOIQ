e0631d341bcb7561db66b33e473c8d3a776832b1
thantrieu <trieuthan92pro@gmail.com>
2023-01-29 22:09:17 +0700

    Add update ex 2.17 + 2.18

--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.17.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.17.kt
@@ -1,2 +1,14 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.showElements() {
+    for (item in this) {
+        print("$item ")
+    }
+}
+
+fun main() {
+    val numbers = arrayOf(0, 144, 1, 233, 2, 3, 5, 8, 13, 89, 55, 34, 16, 21, 101)
+    println("Các phần tử trong mảng:")
+    numbers.sortDESC()
+    numbers.showElements()
+}
--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.18.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.18.kt
@@ -1,2 +1,18 @@
 package net.braniumacademy.exercises15.ex2
 
+fun Array<Int>.swap(index1: Int, index2: Int) {
+    if (index1 in indices && index2 in indices) {
+        val tmp = this[index1]
+        this[index1] = this[index2]
+        this[index2] = tmp
+    }
+}
+
+fun main() {
+    val numbers = arrayOf(0, 144, 1, 233, 2, 3, 5, 8, 13, 89, 55, 34, 16, 21, 101)
+    println("Các phần tử trong mảng gốc:")
+    numbers.showElements()
+    println("Các phần tử trong mảng sau khi đổi chỗ: ")
+    numbers.swap(0, 10)
+    numbers.showElements()
+}

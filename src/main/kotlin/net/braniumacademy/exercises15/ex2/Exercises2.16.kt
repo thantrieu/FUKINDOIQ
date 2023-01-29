c5b9e0c7693c5fc21cfe75b5b6f916f307842cab
thantrieu <trieuthan92pro@gmail.com>
2023-01-29 22:00:45 +0700

    Add update ex 2.15 + 2.16

--- src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.16.kt
+++ src/main/kotlin/net/braniumacademy/exercises15/ex2/Exercises2.16.kt
@@ -1,2 +1,21 @@
 package net.braniumacademy.exercises15.ex2
 
+// hàm sắp xếp các phần tử trong mảng theo thứ tự tăng dần
+fun Array<Int>.sortDESC() {
+    for (i in indices) {
+        for (j in size - 1 downTo i + 1) {
+            if (this[j] > this[j - 1]) {
+                val tmp = this[j]
+                this[j] = this[j - 1]
+                this[j - 1] = tmp
+            }
+        }
+    }
+}
+
+fun main() {
+    val numbers = arrayOf(0, 144, 1, 233, 2, 3, 5, 8, 13, 89, 55, 34, 16, 21, 101)
+    println("Sau sắp xếp:")
+    numbers.sortDESC()
+    println(numbers.contentDeepToString())
+}
